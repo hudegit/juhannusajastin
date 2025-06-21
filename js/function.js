5760cff1b3920a4c88b16ef295c09d7f8f26153b
hudegit <133894785+hudegit@users.noreply.github.com>
2025-06-21 13:37:49 +0300

    Update function.js

--- js/function.js
+++ js/function.js
@@ -1,12 +1,9 @@
 //Juhlinta alkaa
-function createCustomTimer(targetDate, startDate, elementId, expiredMessage) {
+function createCustomTimer(targetDate, elementId, expiredMessage) {
     var countDownDate = new Date(targetDate).getTime()
-    var startTime = new Date(startDate).getTime()
-    var scriptStartTime = Date.now()
     
     var timer = setInterval(function() {
-        var elapsedTime = Date.now() - scriptStartTime
-        var now = startTime + elapsedTime
+        var now = Date.now() // Use actual computer time
         
         var distance = countDownDate - now
         
@@ -27,10 +24,10 @@ function createCustomTimer(targetDate, startDate, elementId, expiredMessage) {
     return timer
 }
 
-// Create all timers
-var timer1 = createCustomTimer("2025-06-19T18:00:00", "2025-06-19T16:30:00", "torstaiStart", "JUHLITAAN")
-var timer2 = createCustomTimer("2025-06-20T14:00:00", "2025-06-19T16:30:00", "perjantaiStart", "NYT JUHLITAAN KOKO PORUKLLA")
-var timer3 = createCustomTimer("2025-06-22T11:00:00", "2025-06-19T16:30:00", "secret", "EN JUO ENÄÄ KOSKAAN &#129314; (paitsi seuraavan kerran)")
+// Create all timers (removed the startDate parameter)
+var timer1 = createCustomTimer("2025-06-19T18:00:00", "torstaiStart", "JUHLITAAN")
+var timer2 = createCustomTimer("2025-06-20T14:00:00", "perjantaiStart", "NYT JUHLITAAN KOKO PORUKLLA")
+var timer3 = createCustomTimer("2025-06-22T11:00:00", "secret", "EN JUO ENÄÄ KOSKAAN &#129314; (paitsi seuraavan kerran)")
 // var timer4 = createCustomTimer("Jun 22, 2025 10:00", "Jun 13, 2025 22:07", "olympialaiset", "Vain parhaat voittaa)")
 
 // ALKUPERÄINEN
